1d6b978828e81d6a3182e0509024c161a3e4db3d
MIKE-HP\MIKE-mini <mike.bskim@gmail.com>
2022-06-23 00:36:51 +0200

    02_validation

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:google_map_test/key.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:http/http.dart' as http;
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 import 'constants/constants.dart';
 
@@ -38,10 +40,10 @@ class MapSampleState extends State<MapSample> {
 
   // 매번 4로 나눠서 나머지 값을 사용
   int _mapType = 0;
-  Set<Marker> _markers = Set();
+  final Set<Marker> _markers = {};
 
   //
-  GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
 
   @override
   void initState() {
@@ -90,8 +92,152 @@ class MapSampleState extends State<MapSample> {
     });
   }
 
-  void _gotoGangnam() {}
+  void _searchPlaces(
+    String locationName,
+    double latitude,
+    double longitude,
+  ) async {
+    setState(() {
+      _markers.clear();
+    });
 
+    final String url =
+        '$baseUrl?key=$API_KEY&location=$latitude,$longitude&radius=1000&language=ko&keyword=$locationName';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      if (data['status'] == 'OK') {
+        GoogleMapController controller = await _controller.future;
+        controller.animateCamera(
+          CameraUpdate.newLatLng(
+            LatLng(latitude, longitude),
+          ),
+        );
+
+        setState(() {
+          final foundPlaces = data['results'];
+
+          for (int i = 0; i < foundPlaces.length; i++) {
+            _markers.add(
+              Marker(
+                markerId: MarkerId(foundPlaces[i]['place_id']),
+                position: LatLng(
+                  foundPlaces[i]['geometry']['location']['lat'],
+                  foundPlaces[i]['geometry']['location']['lng'],
+                ),
+                infoWindow: InfoWindow(
+                  title: foundPlaces[i]['name'],
+                  snippet: foundPlaces[i]['vicinity'],
+                ),
+              ),
+            );
+          }
+        });
+      }
+    } else {
+      debugPrint('Fail to fetch place data');
+    }
+  }
+
+  void _submit() {
+    if (!_fbKey.currentState!.validate()) {
+      return;
+    }
+
+    _fbKey.currentState!.save();
+    final inputValues = _fbKey.currentState!.value;
+    final id = inputValues['placeId'];
+    debugPrint(id);
+
+    final foundPlace = places.firstWhere(
+      (place) => place['id'] == id,
+      // orElse: () => null,
+    );
+
+    debugPrint(foundPlace['placeName']);
+
+    _searchPlaces(foundPlace['placeName']!, 37.53609444, 126.9675222);//37.498295, 127.026437);
+
+    Navigator.of(context).pop();
+  }
+
+  void _gotoGangnam() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          topRight: Radius.circular(15.0),
+        ),
+      ),
+      builder: (context) {
+        return SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 40,
+                    right: 20,
+                    left: 20,
+                    bottom: 20,
+                  ),
+                  child: FormBuilder(
+                    key: _fbKey,
+                    child: Column(
+                      children: <Widget>[
+                        FormBuilderDropdown(
+                          name: 'placeId',
+                          // attribute: 'placeId',
+                          hint: const Text('어떤 장소를 원하세요?'),
+                          decoration: const InputDecoration(
+                            filled: true,
+                            labelText: '장소',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: FormBuilderValidators.required(
+                            errorText: '장소 선택은 필수입니다!',
+                          ),
+                          // validators: [
+                          //   FormBuilderValidators.required(
+                          //     errorText: '장소 선택은 필수입니다!',
+                          //   )
+                          // ],
+                          items: places.map<DropdownMenuItem<String>>(
+                            (place) {
+                              return DropdownMenuItem<String>(
+                                value: place['id'],
+                                child: Text(place['placeName']!),
+                              );
+                            },
+                          ).toList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: _submit,
+                  color: Colors.indigo,
+                  textColor: Colors.white,
+                  child: const Text('Submit'),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
